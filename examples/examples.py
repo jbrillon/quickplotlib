fb54b80a62d6c5af4191b480917bef9389c8bdf9
Maya Tatarelli <49986410+MayaTatarelli@users.noreply.github.com>
2022-07-03 14:29:11 -0400

    fixing bug when plotting single curve; adding example

--- examples/examples.py
+++ examples/examples.py
@@ -1,10 +1,4 @@
 #-----------------------------------------------------
-# Python code for MECH 656: Final Exam
-# - Post-processing code
-# - Written by Julien Brillon
-# - Email: julien.brillon@mail.mcgill.ca
-# - Fall 2021, McGill University
-#-----------------------------------------------------
 # import libraries
 import numpy as np # NumPy: contains basic numerical routines
 import sys
@@ -31,4 +25,21 @@ plotfxn(xdata=[x,x,x,x],
         xlimits=[0,2*np.pi],ylimits=[-2,2],
         which_lines_dashed=[2,3],
         remove_vertical_asymptotes_on_curve_number=[2,3])
+#-----------------------------------------------------
+# Example 2: Trigonometric function (single curve)
+#-----------------------------------------------------
+N = 400
+x = np.linspace(0, 2*np.pi, N)
+y1 = np.sin(x)
+plotfxn(xdata=[x],
+        ydata=[y1],
+        ylabel='$y$',
+        xlabel='$x$',
+        figure_filename='example_02',
+        figure_filetype="png",
+        title_label='Trigonometric Function',
+        legend_labels_tex=['$\\sin(x)$'],
+        black_lines=True,
+        legend_on=False,
+        xlimits=[0,2*np.pi],ylimits=[-1,1])
 #-----------------------------------------------------
